c27ffcfd1eb35a3a45cc3a7e439d4f7d3fb85e34
João Guilherme <126221124+sopena@users.noreply.github.com>
2024-11-02 14:18:12 -0300

    Add files via upload

--- calculadora/main.py
+++ calculadora/main.py
@@ -3,7 +3,7 @@ import flet as ft
 class CalculatorApp(ft.Column):
     def __init__(self):
         super().__init__()
-        self.number_style = ft.ButtonStyle(bgcolor= "#2A2A2C")
+        self.number_style = ft.ButtonStyle(bgcolor= "#2A2A2C", )
         self.operators_style = ft.ButtonStyle(bgcolor= "#FF9F0A")
         self.extra_style = ft.ButtonStyle(bgcolor= "#5C5C5F")
         self.display = ft.Container(
@@ -24,46 +24,46 @@ class CalculatorApp(ft.Column):
                 controls = [
                     ft.Row(
                         controls= [
-                            ft.ElevatedButton("AC", style=self.extra_style),
-                            ft.ElevatedButton("+/-", style=self.extra_style),
-                            ft.ElevatedButton("%", style=self.extra_style),
-                            ft.ElevatedButton("/", style=self.operators_style),
+                            ft.ElevatedButton("AC", style=self.extra_style, width=70),
+                            ft.ElevatedButton("+/-", style=self.extra_style, width=70),
+                            ft.ElevatedButton("%", style=self.extra_style, width=70),
+                            ft.ElevatedButton("/", style=self.operators_style, width=70),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     ),
                     ft.Row(
                         controls= [
-                            ft.ElevatedButton("7", style=self.number_style),
-                            ft.ElevatedButton("8", style=self.number_style),
-                            ft.ElevatedButton("9", style=self.number_style),
-                            ft.ElevatedButton("X", style=self.operators_style),
+                            ft.ElevatedButton("7", style=self.number_style, width=70),
+                            ft.ElevatedButton("8", style=self.number_style, width=70),
+                            ft.ElevatedButton("9", style=self.number_style, width=70),
+                            ft.ElevatedButton("X", style=self.operators_style, width=70),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     ),
                     ft.Row(
                         controls= [
-                            ft.ElevatedButton("4", style=self.number_style),
-                            ft.ElevatedButton("5", style=self.number_style),
-                            ft.ElevatedButton("6", style=self.number_style),
-                            ft.ElevatedButton("-", style=self.operators_style),
+                            ft.ElevatedButton("4", style=self.number_style, width=70),
+                            ft.ElevatedButton("5", style=self.number_style, width=70),
+                            ft.ElevatedButton("6", style=self.number_style, width=70),
+                            ft.ElevatedButton("-", style=self.operators_style, width=70),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     ),
                     ft.Row(
                         controls= [
-                            ft.ElevatedButton("1", style=self.number_style),
-                            ft.ElevatedButton("2", style=self.number_style),
-                            ft.ElevatedButton("3", style=self.number_style),
-                            ft.ElevatedButton("+", style=self.operators_style),
+                            ft.ElevatedButton("1", style=self.number_style, width=70),
+                            ft.ElevatedButton("2", style=self.number_style, width=70),
+                            ft.ElevatedButton("3", style=self.number_style, width=70),
+                            ft.ElevatedButton("+", style=self.operators_style, width=70),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     ),
                     ft.Row(
                         controls= [
-                            ft.IconButton(icon=ft.icons.CALCULATE, style=self.number_style),
-                            ft.ElevatedButton("0", style=self.number_style),
-                            ft.ElevatedButton(",", style=self.number_style),
-                            ft.ElevatedButton(text="=", style=self.operators_style),
+                            ft.IconButton(icon=ft.icons.CALCULATE, style=self.number_style, width=70),
+                            ft.ElevatedButton("0", style=self.number_style, width=70),
+                            ft.ElevatedButton(",", style=self.number_style, width=70),
+                            ft.ElevatedButton(text="=", style=self.operators_style, width=70),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER
                     )
